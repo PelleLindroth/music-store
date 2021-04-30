b48c8ab341379a477dd83055650037c6ef07a038
PelleLindroth <pellelindroth@gmail.com>
2021-04-30 22:28:55 +0200

    Import routes

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ require('dotenv').config()
 const app = express()
 const connect = require('./db/connect')
 const PORT = process.env.PORT
+const AdminRoutes = require('./routes/AdminRoutes')
 const CustomerRoutes = require('./routes/CustomerRoutes')
+const ArtistRoutes = require('./routes/ArtistRoutes')
 const AlbumRoutes = require('./routes/AlbumRoutes')
+const SongRoutes = require('./routes/SongRoutes')
 
 app.use(express.json())
 
-app.use('/api/v1', CustomerRoutes, AlbumRoutes)
+app.use('/api/v1', AdminRoutes, CustomerRoutes, ArtistRoutes, AlbumRoutes, SongRoutes)
 
   ; (async () => {
     await connect()

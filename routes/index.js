6b07c65776c854b7e27c7b054d549f370f647f01
PelleLindroth <pellelindroth@gmail.com>
2021-05-01 14:56:05 +0200

    Refactor route imports

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+const AdminRoutes = require('./AdminRoutes')
+const CustomerRoutes = require('./CustomerRoutes')
+const ArtistRoutes = require('./ArtistRoutes')
+const AlbumRoutes = require('./AlbumRoutes')
+const SongRoutes = require('./SongRoutes')
+
+module.exports = {
+  AdminRoutes,
+  CustomerRoutes,
+  ArtistRoutes,
+  AlbumRoutes,
+  SongRoutes
+}
